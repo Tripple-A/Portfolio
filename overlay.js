e6cd0c7076bb6d739baeafb207c5c5c44940e75a
Tripple-A <biodun9@gmail.com>
2020-04-23 10:47:09 +0100

    added fixed mobile menu tag

--- overlay.js
+++ overlay.js
@@ -1,14 +1,25 @@
-function openNav() {
-    if($(window).width() > 430) {
-        console.log('touched')
-        document.getElementById("myNav").style.width = "70%";
 
+$(window).on('resize', function () {
+    if ($(window).width() < 770) {
+        $('#myNav').addClass('overlay');
+        console.log('horny')
     } else {
-        document.getElementById("myNav").style.width = "100%";
+        $('#myNav').removeClass('overlay');
+        console.log('honey')
+    }
+})
+
+
+function openNav() {
+    console.log('clicked')
+    if ($(window).width() > 430) {
+        document.querySelector('.overlay').style.width = "70%";
+    } else {
+        document.querySelector('.overlay').style.width = "100%";
     }
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+    document.querySelector('.overlay').style.width = "0%";
 }
 
